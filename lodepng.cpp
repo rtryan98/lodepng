c1574d12e372232fd6adb62284472b0357934d5a
Robert Ryan <33040161+rtryan98@users.noreply.github.com>
2021-12-08 05:38:31 +0100

    Changed auto-convert to false

--- lodepng.cpp
+++ lodepng.cpp
@@ -6152,7 +6152,7 @@ void lodepng_encoder_settings_init(LodePNGEncoderSettings* settings) {
   lodepng_compress_settings_init(&settings->zlibsettings);
   settings->filter_palette_zero = 1;
   settings->filter_strategy = LFS_MINSUM;
-  settings->auto_convert = 1;
+  settings->auto_convert = 0;
   settings->force_palette = 0;
   settings->predefined_filters = 0;
 #ifdef LODEPNG_COMPILE_ANCILLARY_CHUNKS
